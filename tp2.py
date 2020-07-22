b3d33308f989bb3067e5386df298abfac2030df3
sofiaoferrari <soferrari@fi.uba.ar>
2020-07-22 18:57:24 -0300

    edicion y json

--- tp2.py
+++ tp2.py
@@ -4,3 +4,45 @@ import csv
 from PIL import Image
 import ipinfo
 import os
+
+
+def edicion_descripcion(descripcion):
+    '''
+    Precondicion: Recibe la descripcion como un string y lo modifica para
+    Postcondicion: Retorna el texto modificado, como string.
+    '''
+    texto = list(descripcion)
+    for i in range(120, len(descripcion), 120):
+        separacion = [' ', '.', ',']
+        while texto[i] not in separacion:
+            i += 1
+        texto.insert(i+1, '\n')
+    texto_final = ''.join(texto)
+    return texto_final
+
+
+def abrir_json(url):
+    '''
+    Precondicion: Recibe un web service como un string e intenta abrirlo, obteniendo su estado.
+    Postcondicion: Retorna un booleano.
+                En el caso de que pueda abrirlo correctamente, retorna el booleano True y en el caso de que
+                surja un error al abrirlo, retorna el booleano False.
+    '''
+    try:
+        respuesta = requests.get(url)
+        respuesta.raise_for_status()
+        funciona = True
+    except requests.exceptions.HTTPError as errh:
+        print("\nHttp Error:\n", errh)
+        funciona = False
+    except requests.exceptions.ConnectionError:
+        print("\nConnecting Error:")
+        print(f'Hubo una falla de conexion a {url}. Verifique su internet o vuelva a intentar mas tarde.')
+        funciona = False
+    except requests.exceptions.Timeout as errt:
+        print("\nTimeout Error:\n", errt)
+        funciona =  False
+    except requests.exceptions.RequestException as err:
+        print("\nOOps: ERROR\n", err)
+        funciona = False
+    return funciona
