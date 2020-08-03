438d3ebaa770e6dee305951dff5b23c6e219b0d3
sofiaoferrari <soferrari@fi.uba.ar>
2020-08-03 14:37:40 -0300

    Ultimas Correcciones Esteticas

--- tp2.py
+++ tp2.py
@@ -53,6 +53,24 @@ def abrir_json(url):
         funciona = False
     return funciona
 
+def volver_a_intentar(valor):
+    '''
+    Precondicion: Le permite al usuario volver a intentar.
+    Postcondicion: Retorna un booleano.
+    '''
+    intentar = False
+    decision = input(f'\nDesea volver a intentar con otra {valor}? (Si/No): ').lower().replace("í", "i")
+    opciones = ['si', 'no', 's', 'n']
+    while decision not in opciones:
+        decision = input('Opción no valida! Vuelva a ingresar. (Si/No): ').lower().replace("í", "i")
+    if decision in ['si', 's']:
+        intentar = True
+    elif decision in ['no', 'n']:
+        intentar = False
+    os.system('cls')
+    titulo()
+    return intentar
+
 def graficar_promedios_anuales(lista_promedios,opcion):
     """
     Precondicion: recibe 2 parámetros, un diccionario con los años y sus promedios de temperatura o humedad por cada dia,
@@ -90,10 +108,10 @@ def mostrar_promedio_anual(lista_promedios,opcion):
     maximo_anio = ""
     if opcion == "3":
         print("\nMilímetros máximos de lluvia de los últimos 5 años.")
-        unidades = " mm"
+        unidades = "mm"
     else:
         print("\nTemperatura máxima de los últimos 5 años.")
-        unidades = " °C"
+        unidades = "°C"
     for anio, datos in lista_promedios.items():
         if int(anio) >= 2015:
             promedio_anio = '%.2f' % (lista_promedios[anio][1] / lista_promedios[anio][0])
@@ -105,6 +123,7 @@ def mostrar_promedio_anual(lista_promedios,opcion):
     continuar = input("\nPresione enter para continuar.")
     os.system('cls')
     titulo()
+    print('\nR E G I S T R O S  A N U A L E S\n')
 
 def guardar_promedios(promedio,anio,lista_promedios):
     """
@@ -153,26 +172,33 @@ def historico_temperatura_humedad():
     Precondición: Le pide al usuario la ruta del archivo csv, y si esta es valida, muestra las
     opciones de los distintos datos del csv.
     """
-    continuar = "si"
-    ruta = input("Introduzca la ruta del archivo CSV en su computador: ")
-    while os.path.exists(ruta) and ruta.endswith(".csv") and continuar == "si":
-        print("\nHistórico de temperatura y humedad de Argentina.\n")
-        print("1)   Gráfico con el promedio de temperaturas anuales de los últimos 5 años.")
-        print("2)   Gráfico con el promedio de humedad de los últimos 5 años.")
-        print("3)   Milímetros máximos de lluvia de los últimos 5 años.")
-        print("4)   Temperatura máxima de los últimos 5 años.")
-        print("5)   Salir.\n")
-        opcion = input("Elija que opción desea ver: ")
-        if opcion != "5" and opcion in ["1","2","3","4"]:
-            cargar_datos_anuales(opcion,ruta)
-        elif opcion == "5":
-            continuar = "no"
-        else:
-            print("Opción no valida. Intente nuevamente.")
-    if os.path.exists(ruta) == False:
-        print("La ruta ingresada no se encuentra en su computador.")
-    elif ruta.endswith(".csv") == False:
-        print("El formato del archivo ingresado es invalido.")
+    continuar = True
+    while continuar is True:
+        print('\nR E G I S T R O S  A N U A L E S\n')
+        ruta = input("Introduzca la ruta del archivo CSV en su computador: ")
+        while os.path.exists(ruta) and ruta.endswith(".csv") and continuar == True:
+            os.system('cls')
+            titulo()
+            print('\nR E G I S T R O S  A N U A L E S\n')
+            print("\nHistórico de temperatura y humedad de Argentina.\n")
+            print("1)   Gráfico con el promedio de temperaturas anuales de los últimos 5 años.")
+            print("2)   Gráfico con el promedio de humedad de los últimos 5 años.")
+            print("3)   Milímetros máximos de lluvia de los últimos 5 años.")
+            print("4)   Temperatura máxima de los últimos 5 años.")
+            print("5)   Volver.\n")
+            opcion = input("Elija que opción desea ver (1-5): ")
+            while opcion not in ["1", "2", "3", "4", "5"]:
+                opcion = input("Opción no valida. Vuelva a intentar (1-5): ")
+            if opcion != "5":
+                cargar_datos_anuales(opcion,ruta)
+            elif opcion == "5":
+                continuar = False
+        if os.path.exists(ruta) == False:
+            print("La ruta ingresada no se encuentra en su computador.")
+            continuar = volver_a_intentar("ruta")
+        elif ruta.endswith(".csv") == False:
+            print("El formato del archivo ingresado es invalido.")
+            continuar = volver_a_intentar("ruta")
 
 def identificar_tormentas(detectados,provincia):
     """
@@ -247,14 +273,27 @@ def datos_radar():
     pergamino = [394,141,"Pergamino, Buenos Aires"]
     santa_rosa = [276,249,"Santa Rosa, La Pampa"]
     provincias = [bahia_blanca,caba,cordoba,mar_del_plata,neuquen,parana,pergamino,santa_rosa]
-    print("Introduzca la ruta de la imagen que desea analizar:")
-    print("-------------(Debe ser un archivo png)-------------")
-    ruta_imagen = input()
-    if os.path.exists(ruta_imagen) and ruta_imagen.endswith(".png"):
-        for i in range(len(provincias)):
-            analisis_radar(provincias[i][0],provincias[i][1],provincias[i][2],ruta_imagen)
-    elif os.path.exists(ruta_imagen) == False:
-        print("La ruta ingresada no se encuentra en su computador.")
+    intentar = True
+    while intentar is True:
+        print('\nA N A L I S I S   R A D A R\n')
+        print("Introduzca la ruta de la imagen que desea analizar:")
+        print("-------------(Debe ser un archivo png)-------------")
+        ruta_imagen = input()
+        if os.path.exists(ruta_imagen) and ruta_imagen.endswith(".png"):
+            os.system('cls')
+            titulo()
+            print('\nA N A L I S I S   R A D A R\n')
+            print('\nA L E R T A S !\n')
+            for i in range(len(provincias)):
+                analisis_radar(provincias[i][0],provincias[i][1],provincias[i][2],ruta_imagen)
+            print('')
+            intentar = False
+        elif os.path.exists(ruta_imagen) == False:
+            print("La ruta ingresada no se encuentra en su computador.")
+            intentar = volver_a_intentar("ruta")
+        elif ruta_imagen.endswith(".png") == False:
+            print("El formato del archivo ingresado es invalido.")
+            intentar = volver_a_intentar("ruta")
 
 def alertas():
     """
@@ -286,6 +325,7 @@ def alertas_local(provincia):
     Precondición: Recibe como parametro una provincia como string.
     Revisa si hay alertas en la provincia actual.
     """
+    print('\nA L E R T A S !\n\n')
     if "Tierra del Fuego" in provincia:
         provincia = 'Tierra del Fuego'
     elif "Capital Federal" in provincia:
@@ -295,7 +335,7 @@ def alertas_local(provincia):
         respuesta = requests.get(url)
         datos = respuesta.json()
         contador_alertas = 0
-        print("\nLista de alertas en la provincia", provincia, ":")
+        print("Lista de alertas en la provincia", provincia, ":")
         for i in range(len(datos)):
             if provincia in str(datos[i]["zones"]):
                 contador_alertas += 1
@@ -305,7 +345,7 @@ def alertas_local(provincia):
         if contador_alertas == 0:
             print(f"\nNo hay ninguna alerta para {provincia} en este momento.")
 
-def validar_coordenada(numero):
+def validar_coordenada(numero, coordenada):
     """
     Precondición: recibe 1 parámetro de tipo string y verifica que contenga al menos 3 caracteres y que no
                     contenga caracteres que no sean numeros.
@@ -316,10 +356,10 @@ def validar_coordenada(numero):
         decimal = True
     except ValueError:
         decimal = False
-    while len(numero) < 3 or decimal == False:
-        print("\nIntroduzca un número de al menos dos digitos:")
+    while len(numero) < 3 or decimal == False or not numero.startswith('-'):
+        print("\nIntroduzca un número negativo de al menos dos dígitos.")
         print("Ejemplo: -34 o -34.2")
-        numero = input()
+        numero = input(f'Ingrese la {coordenada}: ')
         try:
             prueba = float(numero)
             decimal = True
@@ -334,30 +374,32 @@ def geolocalizador():
                 ingresa como parametro en la funcion de alertas locales.
     '''
     url = 'https://ws.smn.gob.ar/map_items/forecast/'
-    latitud = input('Ingrese la latitud: ')
-    latitud = validar_coordenada(latitud)
-    longitud = input('Ingrese la longitud: ')
-    longitud = validar_coordenada(longitud)
-    encontrado = False
-    max_lat = 1
-    max_lon = 1
-    if abrir_json(url):
-        respuesta = requests.get(url)
-        datos = respuesta.json()
-        for i in range(len(datos)):
-            aprox_latitud = abs(float((datos[i]["lat"]).replace("-", "")) - float(latitud.replace("-", "")))
-            aprox_longitud = abs(float((datos[i]["lon"]).replace("-", "")) - float(longitud.replace("-", "")))
-            if aprox_latitud < max_lat and aprox_longitud < max_lon:
-                max_lat = aprox_latitud
-                max_lon = aprox_longitud
-                localizacion = datos[i]
-                encontrado = True
-        if encontrado == True:
-            print(f'\nCiudad encontrada: {localizacion["name"]} ({localizacion["lat"]}, {localizacion["lon"]})')
-            alertas_local(localizacion["province"])
-        elif encontrado == False:
-            print(f"\nNo se encontró la localización para {latitud}, {longitud}.")
-            print("Intente nuevamente.")
+    intentar = True
+    while intentar is True:
+        latitud = input('Ingrese la latitud: ')
+        latitud = validar_coordenada(latitud, 'latitud')
+        longitud = input('Ingrese la longitud: ')
+        longitud = validar_coordenada(longitud, 'longitud')
+        encontrado = False
+        max_lat = 1
+        max_lon = 1
+        if abrir_json(url):
+            respuesta = requests.get(url)
+            datos = respuesta.json()
+            for i in range(len(datos)):
+                aprox_latitud = abs(float((datos[i]["lat"]).replace("-", "")) - float(latitud.replace("-", "")))
+                aprox_longitud = abs(float((datos[i]["lon"]).replace("-", "")) - float(longitud.replace("-", "")))
+                if aprox_latitud < max_lat and aprox_longitud < max_lon:
+                    max_lat = aprox_latitud
+                    max_lon = aprox_longitud
+                    localizacion = datos[i]
+                    encontrado = True
+            if encontrado == True:
+                print(f'\nCiudad encontrada: {localizacion["name"]} ({localizacion["lat"]}, {localizacion["lon"]})')
+                alertas_local(localizacion["province"])
+            elif encontrado == False:
+                print(f"\nNo se encontró la localización para {latitud}, {longitud}.")
+            intentar = volver_a_intentar('coordenada')
 
 def geolocalizador_ip():
     '''
@@ -386,17 +428,25 @@ def menu_alertas():
     Precondicion: Muestra un menu para que el usuario decida que tipo de alerta desea ver.
                 Llama a otras funciones sin parámetros
     '''
+    print('A L E R T A S !')
     print('\n[1] Alertas a Nivel Nacional\n[2] Alertas en mi geolocalización actual')
-    print('[3] Ingresar coordenadas')
-    opcion = input('\nIngrese la opción que desee realizar (1-3): ')
-    while opcion not in ['1', '2','3']:
-        opcion = input('Valor Incorrecto! Por favor, vuelva a ingresar (1-3): ')
+    print('[3] Ingresar coordenadas\n[4] Volver')
+    opcion = input('\nIngrese la opción que desee realizar (1-4): ')
+    while opcion not in ['1', '2', '3', '4']:
+        opcion = input('Valor Incorrecto! Por favor, vuelva a ingresar (1-4): ')
+    os.system('cls')
+    titulo()
     if opcion == '1':
+        print('A L E R T A S !')
         alertas()
     elif opcion == '2':
+        print('\nAlertas en mi geolocalización')
         geolocalizador_ip()
     elif opcion == '3':
+        print('\nAlertas por coordenadas')
         geolocalizador()
+    elif opcion == '4':
+        print('')
 
 def mostrar_pronostico_extendido(dias):
     '''
@@ -431,23 +481,6 @@ def comparar_nombres(nombre_1, nombre_2):
         nombre_conseguido = nombre_1
     return nombre_conseguido
 
-def volver_a_intentar():
-    '''
-    Precondicion: Le permite al usuario volver a buscar el pronostico extendido
-                    para la ciudad que desee.
-    Postcondicion: Retorna un booleano.
-    '''
-    intentar = False
-    decision = input('\nDesea volver a intentar con otra ciudad? (Si/No): ').lower().replace("í", "i")
-    opciones = ['si', 'no', 's', 'n']
-    while decision not in opciones:
-        decision = input('Opción no valida! Vuelva a ingresar. (Si/No): ').lower().replace("í", "i")
-    if decision in ['si', 's']:
-        intentar = True
-    elif decision in ['no', 'n']:
-        intentar = False
-    return intentar
-
 def pronostico_extendido():
     '''
     Precondicion: Le pide al usuario que ingrese la ubicacion de la que desea saber su pronostico
@@ -457,6 +490,7 @@ def pronostico_extendido():
     url = 'https://ws.smn.gob.ar/map_items/forecast/'
     corriendo = True
     while corriendo is True:
+        print('\nP R O N O S T I C O    E X T E N D I D O\n')
         ciudad_elegida = input("Ingrese el nombre de la ciudad que desea ver su pronostico extendido: ").title()
         provincia_elegida = input("Ingrese la provincia en la que se encuentra la ciudad: ").title()
         existe_ciudad_y_provincia = False
@@ -468,7 +502,9 @@ def pronostico_extendido():
             for j in range(len(pronosticos)):
                 ciudad = comparar_nombres(pronosticos[j]['name'], ciudad_elegida)
                 provincia = comparar_nombres(pronosticos[j]['province'], provincia_elegida)
-                if ciudad == pronosticos[j]['name'] and provincia == pronosticos[j]['province']:
+                if ciudad.startswith('Base'):
+                    existe_ciudad_y_provincia = False
+                elif ciudad == pronosticos[j]['name'] and provincia == pronosticos[j]['province']:
                     existe_ciudad_y_provincia = True
                     mostrar_pronostico_extendido(pronosticos[j])
                     provincia_encontrada = pronosticos[j]['province']
@@ -476,7 +512,7 @@ def pronostico_extendido():
             print(f'\nLo sentimos! No se encontraron resultados para {ciudad_elegida}, {provincia_elegida}.')
         elif existe_ciudad_y_provincia is True and conexion is True:
             alertas_local(provincia_encontrada)
-        corriendo = volver_a_intentar()
+        corriendo = volver_a_intentar('ciudad')
 
 def volver_o_salir():
     '''
@@ -485,7 +521,7 @@ def volver_o_salir():
     '''
     volver = False
     print('\n[1] Volver a Menu del Clima\n[2] Salir')
-    eleccion = input('Ingrese la opción que desee (1,2): ')
+    eleccion = input('\nIngrese la opción que desee (1,2): ')
     while eleccion not in ['1', '2']:
         eleccion = input('Valor Incorrecto! Por favor, vuelva a ingresar (1,2): ')
     if eleccion == '1':
@@ -498,8 +534,8 @@ def main():
     programa_corriendo = True
     while programa_corriendo is True:
         titulo()
-        print('\n\nM E N U  D E L  C L I M A')
-        print(f'\n[1] ALERTAS!\n[2] Pronóstico Extendido\n[3] Análisis de Imagen Radar')
+        print('\nM E N U  D E L  C L I M A\n')
+        print(f'[1] ALERTAS!\n[2] Pronóstico Extendido\n[3] Análisis de Imagen Radar')
         print('[4] Histórico de temperaturas y humedad de Argentina\n[5] Salir')
         opcion = input('\nIngrese el numero de la opción que desee (1-5): ')
         while opcion not in ['1', '2', '3', '4', '5']:
@@ -507,19 +543,15 @@ def main():
         os.system('cls')
         titulo()
         if opcion == '1':
-            print('A L E R T A S !')
             menu_alertas()
             programa_corriendo = volver_o_salir()
         elif opcion == '2':
-            print('\nP R O N O S T I C O    E X T E N D I D O\n')
             pronostico_extendido()
             programa_corriendo = volver_o_salir()
         elif opcion == '3':
-            print('\nA N A L I S I S   R A D A R\n')
             datos_radar()
             programa_corriendo = volver_o_salir()
         elif opcion == '4':
-            print('\nR E G I S T R O S  A N U A L E S\n')
             historico_temperatura_humedad()
             programa_corriendo = volver_o_salir()
         elif opcion == '5':
